b5fc6241317c90231084ff874e41e33ad0193a5b
metehanbilir <metehanbilir09@gmail.com>
2023-06-08 20:51:15 -0400

    lesson_12.dart
    Degisken olusturma cesitleri

---
+++ lesson_12.dart
@@ -0,0 +1,68 @@
+void main(){
+
+//Ornek 1:
+var sayi = 10 ;
+int numara = 88;
+
+//Ornek 2:
+var fiyat = 12.99;
+double ucret = 23.59;
+
+print(fiyat);
+print(ucret);
+
+fiyat = 67.99;
+ucret = 10.45;
+
+print(fiyat);
+print(ucret);
+
+//Ornek 3:
+var sayi1 = 10;
+int sayi2 = 20;
+
+var toplam = sayi1 + 50;
+
+int carpma = sayi1 * sayi2 ;
+
+print(toplam);
+print(carpma);
+
+//Ornek 4 :
+var sonuc1;
+int sonuc2;
+
+sonuc1 = 333;
+sonuc2 = 555;
+
+print(sonuc1);
+print(sonuc2);
+
+//Ornek 5:
+var s1,s2,s3;
+s1 = 10;
+s2 = 20;
+s3 = 30;
+
+print("$s1 $s2 $s3");
+
+int k1 = 44,k2 = 78;
+print(k1);
+print(k2);
+
+//Ornek 6:
+var deger = 100;
+deger = 50;
+
+print(deger);
+
+
+
+
+
+
+
+
+
+
+}
