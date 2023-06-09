c84f9faa45834358bd1b6a4dff8e633e2b59ad12
metehanbilir <metehanbilir09@gmail.com>
2023-06-08 20:24:38 -0400

    lesson_11.dart

--- first_lesson.dart
+++ first_lesson.dart
@@ -1,11 +1,7 @@
-void main(){
-
-var ogrenciAdi = "mete" ;
-int     ogrenciYasi = 10 ;
-
-
+void main() {
+  var ogrenciAdi = "mete";
+  int ogrenciYasi = 10;
 
   print(ogrenciAdi);
-print(ogrenciYasi);
-
+  print(ogrenciYasi);
 }
---
+++ lesson_11.dart
@@ -0,0 +1,18 @@
+void main() {
+
+print("Merhaba");
+
+var ad = "Mete";
+int yas = 24;
+
+print(ad);
+print(yas);
+
+print("$ad Bursada $yas yildir yasamaktadir");
+
+
+int a = 10;
+var b = 20;
+
+print("$a ve $b nin toplami : ${a+b}");
+}
