57abb74a87322934c2a38be0d6328882408a58b2
metehanbilir <metehanbilir09@gmail.com>
2023-06-10 00:47:59 -0400

    lesson_15.dart
    Kacis karakterleri ve yorum satirlari

---
+++ lesson_15.dart
@@ -0,0 +1,24 @@
+//kacis karakterleri ve yorum satirlari
+void main(){
+String yazi1 = "Mehmet nasilsin dedi";
+String yazi2 = "Mehmet \"nasilsin\" dedi"; 
+// yaziyi tirnak icinde gostermek icin \" kullanilir.
+print(yazi1);
+print(yazi2);
+
+var yazi3 = "Merhaba bu flutter egitiminde dart dilini ogrenecegiz";
+print(yazi3);//
+var yazi4 =  "Merhaba bu flutter\negitiminde dart dilini ogrenecegiz";
+print(yazi4);// \n sonrasinda gelen metni bi alt satira atar
+var (yazi5) = "Merhaba bu flutter\t egitiminde dart dilini ogrenecegiz";
+print(yazi5);  // \t bir tab kadar bosluk birakir
+
+/* Blok yorum yapma
+blok yorum 1
+blok yorum 2
+ */
+
+
+
+
+}
