17981dd8c2365283d45e142c388b634712dd74f6
metehanbilir <metehanbilir09@gmail.com>
2023-06-10 00:56:08 -0400

    Update lesson_14.dart

--- lesson_14.dart
+++ lesson_14.dart
@@ -6,8 +6,11 @@ final pi = 3.14;
 final String mesaj = "merhaba";
 const y = 20;
 const double a = 20.9;
-
-
-
-
+// asagidaki problems kutusunda  sari unlem hata almamak icin her biri icin print yaptiriyorum.
+print(sayi);
+print(sonuc);
+print(pi);
+print(mesaj);
+print(y);
+print(a);
 }
